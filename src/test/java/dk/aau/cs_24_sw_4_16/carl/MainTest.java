fe34cab029294f73c63a8554240a9d8aab5e095d
DeeKahy <97156953+DeeKahy@users.noreply.github.com>
2024-04-23 16:10:34 +0200

    Revert "getting rid of old testing framework"
    This reverts commit 50beabed10638ee7346b21c63751ee2c721c3127.

---
+++ src/test/java/dk/aau/cs_24_sw_4_16/carl/MainTest.java
@@ -0,0 +1,13 @@
+package dk.aau.cs_24_sw_4_16.carl;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void testIfBooleansAreWorkingProperly()
+    {
+        assertTrue( true );
+    }
+}
