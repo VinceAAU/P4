05cb1e3568680396b2a86b8ce0da3c35317a267f
DeeKahy <97156953+DeeKahy@users.noreply.github.com>
2024-04-23 15:53:57 +0200

    Revert "removed outdated testing framework"
    This reverts commit dc2d2dd7f81cdbfae465ef29ea3fc0a554d9f040.

---
+++ src/test/java/dk/aau/cs_24_sw_4_16/carl/MainTest.java
@@ -0,0 +1,13 @@
+package dk.aau.cs_24_sw_4_16.carl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MainTest {
+    @Test
+    public void testIfBooleansAreWorkingProperly()
+    {
+        assertTrue( true );
+    }
+}
