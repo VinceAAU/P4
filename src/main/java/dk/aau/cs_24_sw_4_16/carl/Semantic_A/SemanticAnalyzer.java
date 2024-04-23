814073dae41b9b8db1400cff0eca956e5d9c2560
CarlHejlesen <113053807+CarlHejlesen@users.noreply.github.com>
2024-04-23 19:39:39 +0200

    Har fundet ud af hvad jeg skal

--- src/main/java/dk/aau/cs_24_sw_4_16/carl/Semantic_A/SemanticAnalyzer.java
+++ src/main/java/dk/aau/cs_24_sw_4_16/carl/Semantic_A/SemanticAnalyzer.java
@@ -1,16 +1,151 @@
 package dk.aau.cs_24_sw_4_16.carl.Semantic_A;
 
 import dk.aau.cs_24_sw_4_16.carl.CstToAst.*;
+
 public class SemanticAnalyzer {
 
-    public boolean printTest =true;
+    public boolean printTest = true;
+
     public void analyze(AstNode root) throws SemanticException {
-      
+        if (printTest) {
+            System.out.println("Hej jeg kommer her ind");
+        }
+
+        if (root != null) {
+            visit(root);
+        }
     }
 
+    private void visit(AstNode node) throws SemanticException {
+        if (node instanceof ProgramNode) {
+            visitProgram((ProgramNode) node);
+            if (printTest) {
+                System.out.println("Hej jeg kommer ind i programnode");
+            }
+        } else if (node instanceof BinaryOperatorNode) {
+            visitBinaryOperator((BinaryOperatorNode) node);
+            if (printTest) {
+                System.out.println("Hej jeg kommer ind i binary");
+            }
+        } else if (node instanceof StatementNode) {
+            visitStatement((StatementNode) node);
+            if (printTest) {
+                System.out.println("Hej jeg kommer ind i statement");
+            }
+        } else if (node instanceof ExpressionNode) {
+            visitExpression((ExpressionNode) node);
+            if (printTest) {
+                System.out.println("Hej jeg kommer ind i expression");
+            }
+        } else if (node instanceof BlockNode) {
+            visitBlock((BlockNode) node);
+            if (printTest) {
+                System.out.println("Hej jeg kommer ind i block");
+            }
+        } else if (node instanceof IfStatementNode) {
+            visitIfStatement((IfStatementNode) node);
+            if (printTest) {
+                System.out.println("Hej jeg kommer ind i if statement");
+            }
+        } else if (node instanceof WhileLoopNode) {
+            visitWhileLoop((WhileLoopNode) node);
+            if (printTest) {
+                System.out.println("Hej jeg kommer ind i while loop");
+            }
+        } else if (node instanceof FunctionDefinitionNode) {
+            visitFunctionDefinition((FunctionDefinitionNode) node);
+            if (printTest) {
+                System.out.println("Hej jeg kommer ind i function definition");
+            }
+        } else if (node instanceof ReturnStatementNode) {
+            visitReturnStatement((ReturnStatementNode) node);
+            if (printTest) {
+                System.out.println("Hej jeg kommer ind i return statement");
+            }
+        } else if (node instanceof FunctionCallNode) {
+            visitFunctionCall((FunctionCallNode) node);
+            if (printTest) {
+                System.out.println("Hej jeg kommer ind i function call");
+            }
+        } else if (node instanceof VariableDeclarationNode) {
+            visitVariableDeclaration((VariableDeclarationNode) node);
+            if (printTest) {
+                System.out.println("Hej jeg kommer ind i variable");
+            }
+        }
+        // Add more as necessary
+    }
 
+    private void visitVariableDeclaration(VariableDeclarationNode node) throws SemanticException {
+        System.out.println(node.toString());
+        visit(node.getValue());
+    }
 
-    
+    private void visitProgram(ProgramNode node) throws SemanticException {
+        for (AstNode statement : node.getStatements()) {
+            visit(statement);
+        }
+    }
+
+    private void visitStatement(StatementNode node) throws SemanticException {
+        // Assuming StatementNode holds a single statement or compound statements
+        visit(node.getNode());
+    }
+
+    private void visitIfStatement(IfStatementNode node) throws SemanticException {
+        // visit(node.getCondition());
+        // visit(node.getThenBlock());
+        // if (node.getElseBlock() != null) {
+        // visit(node.getElseBlock());
+        // }
+    }
+
+    private void visitWhileLoop(WhileLoopNode node) throws SemanticException {
+        // visit(node.getCondition());
+        // visit(node.getBody());
+    }
+
+    private void visitFunctionDefinition(FunctionDefinitionNode node) throws SemanticException {
+        // for (ParameterNode param : node.getParameters()) {
+        // visit(param);
+        // }
+        // visit(node.getBody());
+    }
+
+    private void visitReturnStatement(ReturnStatementNode node) throws SemanticException {
+        // if (node.getExpression() != null) {
+        // visit(node.getExpression());
+        // }
+    }
+
+    private void visitFunctionCall(FunctionCallNode node) throws SemanticException {
+        // for (ExpressionNode argument : node.getArguments()) {
+        // visit(argument);
+        // }
+    }
+
+    private void visitExpression(ExpressionNode node) throws SemanticException {
+        // Assuming ExpressionNode wraps an expression
+        visit(node.getNode());
+    }
+
+    private void visitBlock(BlockNode node) throws SemanticException {
+        for (AstNode statement : node.getStatements()) {
+            visit(statement);
+        }
+    }
+
+    private void visitBinaryOperator(BinaryOperatorNode node) throws SemanticException {
+        // Specific checks for BinaryOperatorNode
+        checkBinaryExpression(node);
+        if (printTest) {
+            System.out.println("Hej jeg kommer int i visitbinary opertor");
+        }
+        // Recursively visit left and right children
+        visit(node.getLeft());
+        visit(node.getRight());
+    }
+
     private void checkBinaryExpression(BinaryOperatorNode node) throws SemanticException {
         AstNode left = node.getLeft();
         AstNode right = node.getRight();
@@ -18,10 +153,14 @@ public class SemanticAnalyzer {
         // Retrieve the types of the left and right operands
         Type leftType = getType(left);
         Type rightType = getType(right);
-
-        // Perform type compatibility check for the specific binary operation
-        if (!isOperationTypeCompatible(leftType, rightType, node.getOperator())) {
-            throw new SemanticException("Type mismatch for operator '" + node.getOperator() + "': " + leftType + " and " + rightType);
+        try {
+            // Perform type compatibility check for the specific binary operation
+            if (!isOperationTypeCompatible(leftType, rightType, node.getOperator())) {
+                throw new SemanticException(
+                        "Type mismatch for operator '" + node.getOperator() + "': " + leftType + " and " + rightType);
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
         }
 
         // If necessary, set the type for the binary expression itself
@@ -54,7 +193,8 @@ public class SemanticAnalyzer {
         } else if (node instanceof IdentifierNode) {
             // Lookup variable type in symbol table
             // Assuming you have a method to get the variable type
-            // return getVariableTypeFromSymbolTable(((VariableReferenceNode) node).getName());
+            // return getVariableTypeFromSymbolTable(((VariableReferenceNode)
+            // node).getName());
         }
         // Handle more node types as necessary
 
