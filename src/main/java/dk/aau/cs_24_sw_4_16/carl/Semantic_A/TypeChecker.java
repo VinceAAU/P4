13435cdb045ebe58f58d4777a684fb752403ef4c
Vincent Kosteyev Bechmann <vbechm22@student.aau.dk>
2024-05-02 10:43:56 +0200

    Consistent-icised case system
    Converted Carl's combination of PascalCase, camelCase, snake_case, and Snake_Pascal_Case into a more consistent system. Also, removed swear words.

--- src/main/java/dk/aau/cs_24_sw_4_16/carl/Semantic_A/TypeChecker.java
+++ src/main/java/dk/aau/cs_24_sw_4_16/carl/Semantic_A/TypeChecker.java
@@ -6,28 +6,28 @@ import java.util.*;
 
 public class TypeChecker {
 
-    HashMap <String, Type> TypeOfReturnFunction;
-    HashMap <String, List<Type>> FunctionParameters;
+    HashMap <String, Type> typeOfReturnFunction;
+    HashMap <String, List<Type>> functionParameters;
 
     //    HashMap<String, FunctionDefinitionNode> fTable; // function table, identifier(x) og node
 
-    HashMap<String, Type> ETable;// variable table, identifier(x) og node(int)
+    HashMap<String, Type> eTable;// variable table, identifier(x) og node(int)
     Stack<HashMap<String, Type>> scopes; // scope table, variable identifier(x) og node
     Deque<Integer> activeScope;// Hvilket scope vi er i nu
     int errorNumber = 1;
-    Boolean print_debugger = false;
-    Boolean has_return_statement = false;
-    String current_active_function ="";
+    Boolean printDebugger = false;
+    Boolean hasReturnStatement = false;
+    String currentActiveFunction ="";
 
     public TypeChecker() {
 //        fTable = new HashMap<>();
-        ETable = new HashMap<>();
+        eTable = new HashMap<>();
         scopes = new Stack<>();
         activeScope = new ArrayDeque<>();
         activeScope.push(0);
-        scopes.add(ETable);
-        TypeOfReturnFunction = new HashMap<>();
-        FunctionParameters = new HashMap<>();
+        scopes.add(eTable);
+        typeOfReturnFunction = new HashMap<>();
+        functionParameters = new HashMap<>();
     }
 
     public void visitor(AstNode node) {
@@ -43,7 +43,7 @@ public class TypeChecker {
     }
 
     public void visitStatements(StatementNode node) {
-        if(print_debugger){
+        if(printDebugger){
             System.out.println("We get in the vist statement");
             System.out.println(node.getClass());
             System.out.println(node.getNode().getClass());
@@ -67,60 +67,60 @@ public class TypeChecker {
             visitFunctionCall((FunctionCallNode) node.getNode());
         }
         if (node.getNode() instanceof ReturnStatementNode){
-            has_return_statement = true;
+            hasReturnStatement = true;
 
-            System.out.println("we get in return statement"+has_return_statement);
+            System.out.println("we get in return statement"+ hasReturnStatement);
             visitReturnNode((ReturnStatementNode) node.getNode());
 
         }
     }
 
-    public void error_handler(String errorMessage){
+    public void errorHandler(String errorMessage){
         System.err.println("Error " + errorNumber);
         errorNumber = errorNumber + 1;
         System.err.println(errorMessage);
     }
-    public Type relationOperator_Type_check(RelationsAndLogicalOperatorNode node) {
+    public Type relationOperatorTypeCheck(RelationsAndLogicalOperatorNode node) {
 
         AstNode left = node.getLeft();
         AstNode right = node.getRight();
 
-        Type left_type = Type.VOID;
-        Type right_Type = Type.VOID;
+        Type leftType = Type.VOID;
+        Type rightType = Type.VOID;
 
-        left_type = getType(left);
-        right_Type = getType(right);
+        leftType = getType(left);
+        rightType = getType(right);
 
-        if (left_type == Type.INT && right_Type == Type.INT) { // Her at udregning sker, som ikke burde ske.
+        if (leftType == Type.INT && rightType == Type.INT) { // Her at udregning sker, som ikke burde ske.
             return Type.BOOLEAN;
-        } else if (left_type == Type.FLOAT && right_Type == Type.FLOAT) {
+        } else if (leftType == Type.FLOAT && rightType == Type.FLOAT) {
             return Type.BOOLEAN;
-        } else if (left_type == Type.BOOLEAN && right_Type == Type.BOOLEAN) {
+        } else if (leftType == Type.BOOLEAN && rightType == Type.BOOLEAN) {
             return Type.BOOLEAN;
         }
         
-        error_handler("Wrong types for relation operation:" + left_type + ":" + left + " And:" + right + ":" + right_Type);
+        errorHandler("Wrong types for relation operation:" + leftType + ":" + left + " And:" + right + ":" + rightType);
 
         return Type.VOID;
     }
 
-    public Type binaryoperator_type_check(BinaryOperatorNode node) {
+    public Type binaryOperatorTypeCheck(BinaryOperatorNode node) {
         AstNode left = node.getLeft(); // Får venstre x som i result=x+y i node form
         AstNode right = node.getRight();// Får højre y i node form
 
-        Type left_type = Type.VOID;
-        Type right_Type = Type.VOID;
+        Type leftType = Type.VOID;
+        Type rightType = Type.VOID;
 
-        left_type = getType(left);
-        right_Type = getType(right);
+        leftType = getType(left);
+        rightType = getType(right);
 
-        if (left_type == Type.INT && right_Type == Type.INT) { // Her at udregning sker, som ikke burde ske.
+        if (leftType == Type.INT && rightType == Type.INT) { // Her at udregning sker, som ikke burde ske.
             return Type.INT;
 
-        } else if (left_type == Type.FLOAT && right_Type == Type.FLOAT) {
+        } else if (leftType == Type.FLOAT && rightType == Type.FLOAT) {
             return Type.FLOAT;
         }
-        error_handler("Wrong types for binnary operation:" + left_type + ":" + left + " And:" + right + ":" + right_Type);
+        errorHandler("Wrong types for binary operation:" + leftType + ":" + left + " And:" + right + ":" + rightType);
         return Type.VOID;
 
     }
@@ -145,11 +145,11 @@ public class TypeChecker {
                 Type assignmentType = getType(ass); // This should give right side type
 
                 if (variableType == assignmentType) {
-                    Type Type_we_save_in_E_table = variableType;
-                    scopes.getLast().put(node.getIdentifier().toString(), Type_we_save_in_E_table);
+                    Type typeWeSaveInETable = variableType;
+                    scopes.getLast().put(node.getIdentifier().toString(), typeWeSaveInETable);
 
                 } else {
-                    error_handler("Tryied to asssign Type:" + assignmentType + " to the variable:" + identifier
+                    errorHandler("Tryied to asssign Type:" + assignmentType + " to the variable:" + identifier
                                                               + " that has the type:" + variableType
                                                               + " And that is hella iligal");
 
@@ -158,7 +158,7 @@ public class TypeChecker {
                 throw new RuntimeException("variable " + node.getIdentifier() + " already exists");
             }
         } catch (Exception e) {
-            error_handler(e.getMessage());
+            errorHandler(e.getMessage());
         }
 
     }
@@ -178,20 +178,20 @@ public class TypeChecker {
     // Check if return = type   er det samme som den function den står i.
     public void visitReturnNode(ReturnStatementNode node) {
       Type returnType = getType(node.getReturnValue());
-      Type activeFunction = TypeOfReturnFunction.get(current_active_function);
-      if(Objects.equals(current_active_function, "")){
-        error_handler("You have made return statement outside a function THAT IS illigal");
+      Type activeFunction = typeOfReturnFunction.get(currentActiveFunction);
+      if(Objects.equals(currentActiveFunction, "")){
+        errorHandler("You have made return statement outside a function THAT IS illigal");
       }
       if(returnType != activeFunction){
-          error_handler("The return type " + returnType + " Does not match the return statement of the function " + activeFunction.getTypeName());
+          errorHandler("The return type " + returnType + " Does not match the return statement of the function " + activeFunction.getTypeName());
       }
     }
 
     public void visitAssignNode(AssignmentNode node) {
-        if(print_debugger){
+        if(printDebugger){
 
         }
-        boolean found_identifier = false;
+        boolean foundIdentifier = false;
         for (HashMap<String, Type> ETable : scopes) {
 
              if(true){
@@ -199,7 +199,7 @@ public class TypeChecker {
                  System.out.println("Etable:"+ETable);
              }
              if (ETable.containsKey(node.getIdentifier().toString())) {// hvis x er i scope
-                 found_identifier = true;
+                 foundIdentifier = true;
                 // tjekke om det er lovligt.
                 // hent gamle type og nye type.
                 Type oldType = getType(node.getIdentifier());
@@ -209,15 +209,15 @@ public class TypeChecker {
                
                 // tjekke om det er lovligt.
                 if (oldType != rightType) {
-                    error_handler("Tryied to asssign Type:" + rightType + " to the variable:" + identifier
+                    errorHandler("Tryied to asssign Type:" + rightType + " to the variable:" + identifier
                                                               + " that has the type:" + oldType
                                                               + " And that is hella iligal");
 
                 }
              } else {
-                error_handler("Variable '" + node.getIdentifier() + "' has not been defined yet.");
+                errorHandler("Variable '" + node.getIdentifier() + "' has not been defined yet.");
              }
-             if (found_identifier){
+             if (foundIdentifier){
                  break;
              }
         }
@@ -231,24 +231,24 @@ public class TypeChecker {
 
     public void visitIfStatement(IfStatementNode node) {
         Type expression = Type.VOID;
-        if(print_debugger){
+        if(printDebugger){
             System.out.println("We get in the If statement");
         }
         
         for (int i = 0; i < node.getExpressions().size(); i++) {
 
-            if(print_debugger){
+            if(printDebugger){
                 System.out.println("We get in first for loop");
                System.out.println("Number of expressions:"+node.getExpressions().size()); 
             }
             expression = getType(node.getExpressions().get(i).getNode());
             if (expression != Type.BOOLEAN) {
-                error_handler("If statements expression must resolve to bool expression, and this resolve to Type:"
+                errorHandler("If statements expression must resolve to bool expression, and this resolve to Type:"
                         + expression);
             }
         }
         for (int i = 0; i < node.getBlocks().size(); i++) {
-            if(print_debugger){
+            if(printDebugger){
                 System.out.println("We get in 2 for loop");
                 System.out.println("Number of blocks:"+node.getBlocks().size()); 
             }
@@ -263,7 +263,7 @@ public class TypeChecker {
     public void visitWhileLoop(WhileLoopNode node) {
         Type toCheck = getType((node.getExpression()).getNode());
         if (toCheck != Type.BOOLEAN) {
-            error_handler("While loop expresion must resolve to bool expresion, and this resolve to Type:" + toCheck);
+            errorHandler("While loop expresion must resolve to bool expresion, and this resolve to Type:" + toCheck);
 
         }
         HashMap<String, Type> localTable = new HashMap<>();
@@ -275,9 +275,9 @@ public class TypeChecker {
 
     public void visitFunctionDefinition(FunctionDefinitionNode node) {
     System.out.println(node+"We get in here");
-        if (!TypeOfReturnFunction.containsKey(node.toString()) && !FunctionParameters.containsKey(node.toString())) {
-            TypeOfReturnFunction.put(node.getIdentifier().toString(), getType(node.getReturnType())); // Key identifier // Type
-            FunctionParameters.put(node.getIdentifier().toString(), new ArrayList<>());
+        if (!typeOfReturnFunction.containsKey(node.toString()) && !functionParameters.containsKey(node.toString())) {
+            typeOfReturnFunction.put(node.getIdentifier().toString(), getType(node.getReturnType())); // Key identifier // Type
+            functionParameters.put(node.getIdentifier().toString(), new ArrayList<>());
 
             HashMap<String, Type> localTable = new HashMap<>();
             scopes.add(localTable);
@@ -286,23 +286,23 @@ public class TypeChecker {
                 String identifierParameter = String.valueOf((parameter.getIdentifier()));  // int carl
                 Type arguementType = getType(parameter.getType());
                 if(localTable.containsKey(identifierParameter) ){
-                    error_handler("The variable:"+identifierParameter+" Already exist in this function parameters.");
+                    errorHandler("The variable:"+identifierParameter+" Already exist in this function parameters.");
                 } else {
                     scopes.getLast().put(identifierParameter, arguementType);
-                    FunctionParameters.get(node.getIdentifier().toString()).add(arguementType);// Den tilføjer typen til listen.  Fn x  tilføjet int x, int x, int x
+                    functionParameters.get(node.getIdentifier().toString()).add(arguementType);// Den tilføjer typen til listen.  Fn x  tilføjet int x, int x, int x
                 }
             }
-            current_active_function = node.getIdentifier().toString();
-            has_return_statement = false;
+            currentActiveFunction = node.getIdentifier().toString();
+            hasReturnStatement = false;
             visitBlockNode(node.getBlock());// Alle statement i fn. En af dem er returnStatement
-            current_active_function = "";
-            if(!has_return_statement && Type.VOID != getType(node.getReturnType())){
-                error_handler("Missing return statement in function declartion:"+node.getIdentifier().toString());
+            currentActiveFunction = "";
+            if(!hasReturnStatement && Type.VOID != getType(node.getReturnType())){
+                errorHandler("Missing return statement in function declartion:"+node.getIdentifier().toString());
             }
-            has_return_statement = false;
+            hasReturnStatement = false;
             scopes.remove(localTable);
         } else {
-            error_handler("function " + node.getIdentifier() + " already exists");
+            errorHandler("function " + node.getIdentifier() + " already exists");
         }
     }
         /*
@@ -315,23 +315,23 @@ public class TypeChecker {
         if(!Objects.equals(node.getFunctionName().toString(), "print")) {
             HashMap<String, Type> localETable = new HashMap<>();
             scopes.add(localETable);
-            if (TypeOfReturnFunction.containsKey(node.getFunctionName().toString())) {
-                List<Type> expected_Parameter_Types = FunctionParameters.get(node.getFunctionName().toString());
+            if (typeOfReturnFunction.containsKey(node.getFunctionName().toString())) {
+                List<Type> expectedParameterTypes = functionParameters.get(node.getFunctionName().toString());
 
-                int expected_arguments_size = expected_Parameter_Types.size();
-                int actual_arguments_size = node.getArguments().size();
-                if(expected_arguments_size != actual_arguments_size){
-                    error_handler("Function Expected:"+expected_arguments_size+" Arguments but got :"+actual_arguments_size+" Arguments");
+                int expectedArgumentsSize = expectedParameterTypes.size();
+                int actualArgumentsSize = node.getArguments().size();
+                if(expectedArgumentsSize != actualArgumentsSize){
+                    errorHandler("Function Expected:"+expectedArgumentsSize+" Arguments but got :"+actualArgumentsSize+" Arguments");
                 }
 
-                for (int i = 0; i < expected_arguments_size; i++) {
-                    if (expected_Parameter_Types.get(i) != getType(node.getArgument(i))) {
-                        error_handler("Function Expected type: " + expected_Parameter_Types.get(i) + ", as "+ i + " Argument but got " + getType(node.getArgument(i)));
+                for (int i = 0; i < expectedArgumentsSize; i++) {
+                    if (expectedParameterTypes.get(i) != getType(node.getArgument(i))) {
+                        errorHandler("Function Expected type: " + expectedParameterTypes.get(i) + ", as "+ i + " Argument but got " + getType(node.getArgument(i)));
                     }
                 }
 
             } else {
-                error_handler("The function :" + node.getFunctionName().toString() + " May not exist or be out of scope.");
+                errorHandler("The function :" + node.getFunctionName().toString() + " May not exist or be out of scope.");
             }
         }
     }
@@ -353,12 +353,12 @@ public class TypeChecker {
             }
 
         } else if (node instanceof BinaryOperatorNode) {
-            type = binaryoperator_type_check((BinaryOperatorNode) node);
+            type = binaryOperatorTypeCheck((BinaryOperatorNode) node);
         } else if (node instanceof RelationsAndLogicalOperatorNode) {
-            type = relationOperator_Type_check((RelationsAndLogicalOperatorNode) node);
+            type = relationOperatorTypeCheck((RelationsAndLogicalOperatorNode) node);
         } else if (node instanceof  FunctionCallNode){
-            String variable_name = ((FunctionCallNode) node).getFunctionName().toString();
-            type = TypeOfReturnFunction.get(variable_name);
+            String variableName = ((FunctionCallNode) node).getFunctionName().toString();
+            type = typeOfReturnFunction.get(variableName);
         }
 
         else if (node instanceof IntNode) {
@@ -372,8 +372,8 @@ public class TypeChecker {
         } else if (node instanceof StringNode) {
             type = Type.STRING;
         } else if (node instanceof TypeNode) {
-            String type_fuck_me_why_did_we_save_types_as_String = ((TypeNode) node).getType();
-            switch (type_fuck_me_why_did_we_save_types_as_String) {
+            String typeAsString = ((TypeNode) node).getType();
+            switch (typeAsString) {
                 case "int":
                     return Type.INT;
                 case "string":
@@ -397,7 +397,7 @@ public class TypeChecker {
             }
 
         } else {
-            error_handler("The node we get failed to handle:" + node.getClass());
+            errorHandler("The node we get failed to handle:" + node.getClass());
         }
         return type;
     }
